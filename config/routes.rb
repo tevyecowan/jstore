d945a0d6f3f74dd293dcc7e6adee87b39d1eae4f
Tevye Cowan <t.b.cowan@gmail.com>
2023-08-08 20:10:34 -0700

    basic functionality and basic ui

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :deposits
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'show_item', to: 'items#index'
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
