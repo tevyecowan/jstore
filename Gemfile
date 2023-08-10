44fa3b71893c3718a7688bc064d21c23935edf9a
Tevye Cowan <t.b.cowan@gmail.com>
2023-08-09 17:01:51 -0700

    adding aws

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem "sprockets-rails"
 # postgres
 gem "pg"
 
+# using aws for storage
+gem "aws-sdk-s3", require: false
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # setting active storage service to amazon
+  config.active_storage.service = :amazon
 
   # Code is not reloaded between requests.
   config.cache_classes = true
