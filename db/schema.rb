0d9e19011942bd2923226f8ce601b7e34778a613
Tevye Cowan <t.b.cowan@gmail.com>
2023-08-09 17:08:44 -0700

    change uploads destination

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_08_014605) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "deposits", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -19,6 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_014605) do
     t.text "notes"
     t.index ["item_id"], name: "index_deposits_on_item_id"
     t.index ["user_id"], name: "index_deposits_on_user_id"
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
